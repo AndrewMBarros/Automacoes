de32013b7f524738d862b8bf04cea7d56d8b113e
AndrewMBarros <49644696+AndrewMBarros@users.noreply.github.com>
2024-01-06 23:51:12 -0300

    Update main.py

--- Levantamento de Preços - Amazon/main.py
+++ Levantamento de Preços - Amazon/main.py
@@ -1,21 +1,22 @@
-### melhorar para testar o erro de precisar encontra os elementos
-### melhorar tbm para caso exista o arquivo na pasta, ele apagar e substituir
-
-### colocar um executavel e instalador tambem
-
-
-
 #selenium para o site
 #openpyxl para ler as tabelas
-
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import openpyxl
 
+# Verificar se o arquivo existe
 
+
+if os.path.exists('produtos.xlsx'):
+    # Se existir, apagar o arquivo
+    os.remove('produtos.xlsx')
+    print("Arquivo 'produtos.xlsx' existente removido com sucesso!")
+    
 driver = webdriver.Chrome()
 driver.get('https://www.amazon.com.br/gp/bestsellers/computers/16364756011?ref_=Oct_d_obs_S&pd_rd_w=0rJIH&content-id=amzn1.sym.ddac7217-197b-4ce9-8f99-a40c3c6e5f1b&pf_rd_p=ddac7217-197b-4ce9-8f99-a40c3c6e5f1b&pf_rd_r=GHXMEA6E079QX0R5PVN5&pd_rd_wg=lsQPP&pd_rd_r=a1b5bb92-3a1c-44f8-b92f-3afe683a12a5')
-
+#input()
+driver.close()
 #se desejar o site nao feche basta colocar
 #input()
 #nome do produto
@@ -39,12 +40,8 @@ sheet_produtos['A1'].value = 'Produto'
 sheet_produtos['B1'].value = 'Preço'
 #salvar na planilha
 
-
 for titulo, preco in zip (titulos, precos):
     sheet_produtos.append([titulo.text, preco.text])
 #.text pq deseja apenas a propriedade texto daquele elemento
-
+ 
 workbook.save('produtos.xlsx')
-
-#para deixar a pagina aberta  basta colocar o input 
-#input()
